8d77511a9b8ef9110f4c7a5a7245dda28400dec2
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-17 09:34:34 +0300

    Commit 2

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,39 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+
+
+  fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(response => response.json())
+    .then(data => {
+
+      // console.log(data.results)
+
+      // let pokemons = []
+      let num = 0
+
+
+      for (var name in data.results) {
+        console.log(data.results[name].name)
+        // pokemons.push
+
+        if(num < 10){
+          let a = document.createElement('li')
+          a.innerHTML = data.results[name].name;
+          ul.appendChild(a)
+
+          num++;
+
+        }
+
+
+
+
+     
+
+       
+
+      }
+
+    });
 });
