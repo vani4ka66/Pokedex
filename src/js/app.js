ca3a761b39a02939ed8cb05c9d485192888a2bbb
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-17 09:39:55 +0300

    Commit 3

--- src/js/app.js
+++ src/js/app.js
@@ -10,31 +10,21 @@ window.addEventListener("DOMContentLoaded", () => {
     .then(response => response.json())
     .then(data => {
 
-      // console.log(data.results)
-
-      // let pokemons = []
       let num = 0
 
 
       for (var name in data.results) {
         console.log(data.results[name].name)
-        // pokemons.push
 
-        if(num < 10){
+        if (num < 10) {
           let a = document.createElement('li')
-          a.innerHTML = data.results[name].name;
+          a.innerText = data.results[name].name;
+
           ul.appendChild(a)
 
           num++;
 
         }
-
-
-
-
-     
-
-       
 
       }
 
